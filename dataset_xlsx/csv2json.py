cad2a2929bf777fe276cbcbf71af5d98f05becbe
Andy Judson <andy.judson@gmail.com>
2013-05-03 00:09:01 +0100

    Revised data files

--- dataset_xlsx/csv2json.py
+++ dataset_xlsx/csv2json.py
@@ -12,12 +12,21 @@ parser.add_argument('-i', '--ifilename', help='input filename', required=True)
 parser.add_argument('-o', '--ofilename', help='output filename', default='dump.json')
 args = parser.parse_args()
 
+# define function to parse strings to numbers
+# not working! - add param 'cls=ParseString' to the json.dump call
+class ParseString(json.JSONEncoder):
+	def default(self, obj):
+	    try:
+	    	return float(obj)
+	    except Exception:
+	        return obj
+
 # convert file format...
 with open(args.ifilename, 'rU') as ifile:
 	print "Opening CSV file: ", args.ifilename 
 	reader = csv.DictReader(ifile)
 	print "Saving JSON to file: ", args.ofilename
 	ofile = open(args.ofilename,'w') 
-	ofile.write(json.dumps([r for r in reader])) 
+	ofile.write(json.dumps([r for r in reader], sort_keys=True, indent=4)) 
  	ifile.close()
 	ofile.close()
